9e2f23788f54c82b1325a45100f7216e4d35c318
fably <fably@DESKTOP-UKSQ2UV>
2016-05-30 15:24:15 +0200

    Aggiunta ricerca utente

--- core/view/header.php
+++ core/view/header.php
@@ -155,7 +155,15 @@
                         </div>
                     </div>
 
-
+                    <form action="#" method="get" class="sidebar-form">
+                        <div class="input-group">
+                            <input type="text" name="q" class="form-control" placeholder="Ricerca Utente...">
+                            <span class="input-group-btn">
+                                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                                </button>
+                            </span>
+                        </div>
+                    </form>
 
                     <!-- Sidebar Menu -->
                     <ul class="sidebar-menu">
